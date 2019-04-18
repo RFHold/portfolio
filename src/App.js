b3edfdc0f40a10a2d7fe6b5ef42782a7a07255a8
Robert Holden <robert@robhold.com>
2019-04-18 01:56:02 -0400

    Started filling out actual content

--- src/App.js
+++ src/App.js
@@ -46,10 +46,9 @@ class App extends Component {
             <h3>About Me</h3>
             <hr />
             <img src="/images/Headshot small.jpg" alt="Headshot" className="inlineImage"></img>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates, 
+              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and 
+              deplay an internal managment application. I love the problem solving aspect of programming, I enjoy taking an issue and breaking it down into it most basic steps and then building a solution.
             </p>
           </section>
           <section ref={(el) => this.portfolioSection = el} id="portfolioSection" className="card">
