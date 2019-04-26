4e7887f153bd9a41d0365476429727079149fa24
Robert Holden <robert@robhold.com>
2019-04-26 12:44:17 -0400

    portfolio section work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles/reset.css';
-import './styles/grid.css';
+import './styles/bootstrap-grid.css';
 import './styles/typography.css';
 import './styles/app.css';
 
@@ -12,7 +12,7 @@ class App extends Component {
     this.handleScroll = this.handleScroll.bind(this);
 
     this.state = {
-      themeAmbiance: 'blue',
+      themeAmbiance: 'purple',
     };
   }
   componentDidMount() {
@@ -29,7 +29,7 @@ class App extends Component {
         <header>
           <div className="wrapper">
             <div className="headerBrand">
-              <h2>Robert Holden</h2>
+              <h1>Robert Holden</h1>
             </div>
             <nav className="headerNav">
               <ul>
@@ -43,25 +43,43 @@ class App extends Component {
         </header>
         <div className="wrapper">
           <section ref={(el) => this.aboutMeSection = el} id="aboutMeSection" className="card">
-            <h3>About Me</h3>
+            <h2>About Me</h2>
             <hr />
             <img src="/images/Headshot small.jpg" alt="Headshot" className="inlineImage"></img>
-            <p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates, 
-              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and 
+            <p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates,
+              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and
+              deplay an internal managment application. I love the problem solving aspect of programming, I enjoy taking an issue and breaking it down into it most basic steps and then building a solution.
+            </p><p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates,
+              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and
               deplay an internal managment application. I love the problem solving aspect of programming, I enjoy taking an issue and breaking it down into it most basic steps and then building a solution.
             </p>
           </section>
           <section ref={(el) => this.portfolioSection = el} id="portfolioSection" className="card">
-            <h3>Portfolio</h3>
+            <h2>Portfolio</h2>
             <hr />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div className="row">
+              <div className="col-4">
+                <img src="http://via.placeholder.com/300" className="projectImage" alt="project"></img>
+              </div>
+              <div className="col-8">
+                <div className="projectDescription">
+                  <h3>Project</h3>
+                  <p>project description that describes a describable project.
+                    project description that describes a describable project.
+                  project description that describes a describable project.
+                  project description that describes a describable project.
+                  project description that describes a describable project.
+                  project description that describes a describable project.
+                  project description that describes a describable project.
+                  project description that describes a describable project.
+                  </p>
+                  <a href="./" className="button">Live Project</a>
+                </div>
+              </div>
+            </div>
           </section>
           <section ref={(el) => this.experienceSection = el} id="experienceSection" className="card">
-            <h3>Experience</h3>
+            <h2>Experience</h2>
             <hr />
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -70,7 +88,7 @@ class App extends Component {
             </p>
           </section>
           <section id="contactSection" className="card">
-            <h3>Contact</h3>
+            <h2>Contact</h2>
             <hr />
             <p><strong>Email : </strong><a href="mailto:robert@robhold.com">robert@robhold.com</a></p>
             <p><strong>Phone : </strong><a href="tel:+14045029135">(404) 502-9135</a></p>
