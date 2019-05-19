9b246594c416fcee2f3191f1c8a170e8fc65da7a
RFHold <Robert@Robhold.com>
2019-05-18 22:05:16 -0400

    added portfolio item and finalized about me

--- src/App.js
+++ src/App.js
@@ -46,40 +46,42 @@ class App extends Component {
             <h2>About Me</h2>
             <hr />
             <img src="/images/Headshot small.jpg" alt="Headshot" className="inlineImage"></img>
-            <p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates,
-              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and
-              deplay an internal managment application. I love the problem solving aspect of programming, I enjoy taking an issue and breaking it down into it most basic steps and then building a solution.
-            </p><p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowlage gained from previouse projects and jobs and help launch my career. Starting off building CSS templates,
-              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transisioned to a position in the company in which I was colaberating with a team to design, build and
-              deplay an internal managment application. I love the problem solving aspect of programming, I enjoy taking an issue and breaking it down into it most basic steps and then building a solution.
+            <p>With a passion for solving problems I started my career path early in highschool with my first job building website templates. I enjoy the logistical aspect of programming with every day being a new opportunity
+               for something great. I am a quick and focused learner who actively looks and avoids complications before they become a roadblock. I am looking for a place to let me express my best concepts and use my skills to their fullest potential.</p><br/>
+            <p>I am currently attending Georgia Tech's Fullstack coding bootcamp. This course will refine my current knowledge gained from previous projects and jobs and help launch my career. I started off building CSS templates, and then
+              I quickly moved on to more complicated tasks. I started building custom modules for customer sites. Eventually I transitioned to a position in the company in which I was collaborating with a team to design, build and
+              deploy an internal management application.
             </p>
           </section>
           <section ref={(el) => this.portfolioSection = el} id="portfolioSection" className="card">
             <h2>Portfolio</h2>
             <hr />
             <div className="row">
-              <div className="col-4">
-                <img src="http://via.placeholder.com/300" className="projectImage" alt="project"></img>
-              </div>
-              <div className="col-8">
-                <div className="projectDescription">
-                  <h3>Project</h3>
-                  <p>project description that describes a describable project.
-                    project description that describes a describable project.
-                  project description that describes a describable project.
-                  project description that describes a describable project.
-                  project description that describes a describable project.
-                  project description that describes a describable project.
-                  project description that describes a describable project.
-                  project description that describes a describable project.
+              <div className="col-12">
+                <div className="projectContainer">
+                  <img src="/images/watchpad-logo.jpg" className="projectImage inlineImage small" alt="watchPad Logo"></img>
+                  <h3>WatchPad</h3>
+                  <br />
+                  <p>
+                    WatchPad is a simple tool which allows the user to organize and keep track of movies and tv shows that they want to watch. Combining apis such as The Movie Database and TV Database to allow the user to search the ever expanding collection
+                    of todays media. With future plans to integrate with media servers such as Plex and Emby, WatchPad will be a great asset to anyone looking to create their own media server.                   
                   </p>
-                  <a href="./" className="button">Live Project</a>
+                  <a href="https://rfhold.github.io/bc-project-1/" className="button">Live Project</a>
                 </div>
               </div>
             </div>
           </section>
           <section ref={(el) => this.experienceSection = el} id="experienceSection" className="card">
             <h2>Experience</h2>
+            <hr />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </section>
+          <section ref={(el) => this.skillsSection = el} id="skillsSection" className="card">
+            <h2>Skills</h2>
             <hr />
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -118,6 +120,8 @@ class App extends Component {
       color = 'green';
     } else if (this.hasBeenScrolledPast(this.portfolioSection, offset)) {
       color = 'blue';
+    }else{
+      color = 'purple';
     };
 
     this.setState({
