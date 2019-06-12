bb36825bc85989c3fb5c003488a2c4ea5e0acc4d
Robert Holden <robert@robhold.com>
2019-06-12 10:26:58 -0400

    portfolio update

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
 						<h2>About Me</h2>
 						<hr />
 						<p>
-							With a passion for solving problems I started my career path early in highschool with my first job building website templates. I enjoy the logistical aspect of programming with every day being a new opportunity for something great. I am a quick and focused learner who actively looks and avoids complications before they become a roadblock. I am looking for a place to let me express my best concepts and use my skills to their fullest potential.
+							With a passion for solving problems I started my career path with my first job building website templates. I enjoy the logistical aspect of programming with every day being a new opportunity for something great. I am a quick and focused learner who actively looks and avoids complications before they become a roadblock. I am looking for a place to let me express my best concepts and use my skills to their fullest potential.
 
 						</p>
 					</section>
@@ -56,7 +56,7 @@ class App extends Component {
 						<div className="row">
 							<div className="col-12">
 								<div className="subContainer">
-									<img src="/images/watchpad-logo.jpg" className="projectImage inlineImage small" alt="watchPad Logo"></img>
+									<img src="/images/watchpad.jpg" className="projectImage inlineImage small" alt="watchPad Logo"></img>
 
 									<div className="flexBetween">
 										<h3>WatchPad</h3>
@@ -65,21 +65,45 @@ class App extends Component {
 									<p>
 										WatchPad is a simple tool which allows the user to organize and keep track of movies and tv shows that they want to watch. Combining apis such as The Movie Database and TV Database to allow the user to search the ever expanding collection
 										of todays media. With future plans to integrate with media servers such as Plex and Emby, WatchPad will be a great asset to anyone looking to create their own media server.</p>
+									<a href="https://rfhold.github.io/bc-project-1/">Link to project</a>
 								</div>
 							</div>
 						</div>
+						<div className="row">
+							<div className="col-12">
+								<div className="subContainer">
+									<img src="/images/dialog.jpg" className="projectImage inlineImage small" alt="DiaLOG Logo"></img>
+
+									<div className="flexBetween">
+										<h3>DiaLOG</h3>
+										<a href="https://dialog-application.herokuapp.com/" className="button">Live Project</a>
+									</div>
+									<p>DiaLOG is a web based group chat application. DiaLOG allows you to create groups of people and organize your topics with channels.
+										With the power of Web Sockets, DiaLOG facilitates instant communication for easy of use.</p>
+									<p>DiaLOG was build using Node.js, Express, Web Sockets, Bootstrap, PUG, MySQL, and a few supporting packages.</p>
+									<a href="https://dialog-application.herokuapp.com/">Link to project</a>
+								</div>
+							</div>
+						</div>
+						{/* <div className="row">
+							<div className="col-12">
+								<div className="subContainer">
+									<img src="/images/newsscraper.jpg" className="projectImage inlineImage small" alt="News Scraper Logo"></img>
+
+									<div className="flexBetween">
+										<h3>News Scraper</h3>
+										<a href="https://news-scraper-bcs-app.herokuapp.com/" className="button">Live Project</a>
+									</div>
+									<p>This app is a simple demonstration of the power of website scraping. This scraper takes news articles from Motor Trend and stores their titles, excerpts, and links to the full article.
+										This can be useful to store and organize related content such as; notes, favorites, and categories.
+									</p><a href="https://news-scraper-bcs-app.herokuapp.com/">Link to project</a>
+								</div>
+							</div>
+						</div> */}
 					</section>
 					<section ref={(el) => this.experienceSection = el} id="experienceSection" className="card">
 						<h2>Experience</h2>
 						<hr />
-						<div className="subContainer">
-							<div className="flexBetween">
-								<h3>501CiO Consulting</h3> <sub className="stamp">3 Years <strong>Fullstack Developer</strong></sub>
-							</div>
-							<p>
-								I started work at 501CiO making css templates for client websites. Six months later I began developing a time-sheet and invoicing system for internal use. During development I got ample experience with languages such as PHP, Ruby,
-								 and JavaScript. I also utilized MySQL, Rails, Node.js, Angular.js and many other technologies. As lead developer, I used my creative thinking and ambitious personality to solve problems and fabricate a great user experience.</p>
-						</div>
 						<div className="subContainer">
 							<div className="flexBetween">
 								<h3>Cellairis</h3> <sub className="stamp" >8 Months <strong>Lead Sales/Repair Tech</strong></sub>
@@ -88,6 +112,14 @@ class App extends Component {
 								At Cellairis I was charged with repairing customer devices which included replacing or repairing components, troubleshooting software issues, data recovery, and cleaning PCs. On top of being a repair tech, I was also Lead Sales.
 												As the Lead Sales Associate I communicated with customers about their repair and order status as well as maintain the store to both optimize workflow and display our products effectively. My time at Cellairis tested my ability
 								to multitask and communicate with the rest of my team.</p>
+						</div>
+						<div className="subContainer">
+							<div className="flexBetween">
+								<h3>501CiO Consulting</h3> <sub className="stamp">3 Years <strong>Fullstack Developer</strong></sub>
+							</div>
+							<p>
+								I started work at 501CiO making css templates for client websites. Six months later I began developing a time-sheet and invoicing system for internal use. During development I got ample experience with languages such as PHP, Ruby,
+								 and JavaScript. I also utilized MySQL, Rails, Node.js, Angular.js and many other technologies. As lead developer, I used my creative thinking and ambitious personality to solve problems and fabricate a great user experience.</p>
 						</div>
 						<div className="subContainer">
 							<div className="flexBetween">
@@ -126,73 +158,57 @@ class App extends Component {
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>JavaScript</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
-									<h4>PHP</h4>
-									<code>9/10</code>
+									<h4>PHP</h4>								
 								</div>
 								<div className="flexBetween">
-									<h4>Java</h4>
-									<code>7/10</code>
+									<h4>Java</h4>								
 								</div>
 								<div className="flexBetween">
-									<h4>Ruby</h4>
-									<code>4/10</code>
+									<h4>Ruby</h4>								
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>HTML</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
 									<h4>CSS</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
-									<h4>SASS</h4>
-									<code>9/10</code>
+									<h4>SASS</h4>								
 								</div>
 								<div className="flexBetween">
-									<h4>Typescript</h4>
-									<code>6/10</code>
+									<h4>Typescript</h4>								
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Node.js</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
-									<h4>Ajax</h4>
-									<code>10/10</code>
+									<h4>Angular</h4>
 								</div>
 								<div className="flexBetween">
-									<h4>Express</h4>
-									<code>9/10</code>
+									<h4>Express</h4>								
 								</div>
 								<div className="flexBetween">
-									<h4>React</h4>
-									<code>8/10</code>
+									<h4>React</h4>								
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Git</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
 									<h4>REST</h4>
-									<code>10/10</code>
 								</div>
 								<div className="flexBetween">
-									<h4>MySQL</h4>
-									<code>8/10</code>
+									<h4>MySQL</h4>								
 								</div>
 								<div className="flexBetween">
-									<h4>MongoDB</h4>
-									<code>7/10</code>
+									<h4>MongoDB</h4>								
 								</div>
 							</div>
 						</div>
@@ -201,25 +217,21 @@ class App extends Component {
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Creative</h4>
-									<code>10/10</code>
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Ambitious</h4>
-									<code>10/10</code>
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Dedicated</h4>
-									<code>10/10</code>
 								</div>
 							</div>
 							<div className="col-6 col-md-3">
 								<div className="flexBetween">
 									<h4>Communication</h4>
-									<code>10/10</code>
 								</div>
 							</div>
 						</div>
